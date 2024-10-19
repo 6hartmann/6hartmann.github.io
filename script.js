fee91b8a44626db910f334f76e1fc92e41a1bd46
6hartmann <business@okaycha.mp>
2024-10-19 13:44:09 +0200

    feat: bo6 time, icons, colors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDown = new Date("dec 15, 2023 10:10:10").getTime();
+var countDown = new Date("2024-10-24T13:00:00.000+02:00").getTime();
 // atualiza a contagem a cada 1s
 var updateCount = setInterval(() => {
   // pegar data e hora atual
@@ -6,9 +6,13 @@ var updateCount = setInterval(() => {
   //calcula o tempo entre a data atual e a contagem
   var distCalc = countDown - dataTimeNow;
   var days = Math.floor(distCalc / (1000 * 60 * 60 * 24));
+  days = days < 10 ? "0"+days : days;
   var hours = Math.floor((distCalc % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  hours = hours < 10 ? "0"+hours : hours;
   var minutes = Math.floor((distCalc % (1000 * 60 * 60)) / (1000 * 60));
+  minutes = minutes < 10 ? "0"+minutes : minutes;
   var seconds = Math.floor((distCalc % (1000 * 60)) / 1000);
+  seconds = seconds < 10 ? "0"+seconds : seconds;
   // mostra a contagem no elemento <p class="timer"></p>
   document.querySelector(".days-time").innerHTML = days;
   document.querySelector(".hours-time").innerHTML = hours;
